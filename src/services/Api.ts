16e845925a0d37a9be059538f07e66cd491e1eff
rodrigo brinate <rbp20199@gmail.com>
2022-08-20 06:56:09 -0300

    fix deploy

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -5,5 +5,5 @@ let Api
 export default  Api = axios.create({ 
       //  baseURL: "http://localhost:3001",
         //baseURL: "https://chat-in-realitime.herokuapp.com"
-        baseURL: process.env.SERVE
+        baseURL: process.env.SERVER_HOST
       });
